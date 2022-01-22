90f22fde8bfd7f7ca1c1e4517daa06d3ba5dcc8c
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-01-22 19:24:55 +0100

    :rotating_light: fix linting

--- src/app/Services/authentification.service.ts
+++ src/app/Services/authentification.service.ts
@@ -1,48 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
 
-  constructor(private httpClient:HttpClient, private router : Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
   private  baseUrl = 'http://localhost:3000/auth/client/';
-  
-  
+
+
   // call for authentication backend api with a loginRequest in parameter(email + motDePasse)
-   authenticate(loginRequest) {    
-    return this.httpClient.post<any>(this.baseUrl+'login',loginRequest)
+  authenticate(loginRequest): Observable<any> {
+    return this.httpClient.post<any>(this.baseUrl  + 'login', loginRequest);
   }
 
     // call for authentication backend api with a registerRequest in parameter(email + motDePasse)
-    register(registerRequest) {    
-      return this.httpClient.post<any>(this.baseUrl+'signup',registerRequest)
+    register(registerRequest): Observable<any> {
+      return this.httpClient.post<any>(this.baseUrl + 'signup', registerRequest);
     }
 
   // call for authentication backend api with a loginRequest in parameter(username + motDePasse)
-  getRoles(loginRequest) {
+  getRoles(loginRequest): Observable<any> {
       const headers = new HttpHeaders({
-        
-        'Authorization':localStorage.getItem('token')
-      })
-      
+        Authorization: localStorage.getItem('token')
+      });
     // recuperation du role avec login
-    return  this.httpClient.get<any>(this.baseUrl+'email/'+loginRequest.email,{headers : headers})    
-  }
-      
-  isUserLoggedIn() {
-    let user = localStorage.getItem('email')
-
-    //return true if user not null
-    return !(user === null)
+      return this.httpClient.get<any>(this.baseUrl + 'email/' + loginRequest.email, {headers});
   }
 
-  logOut() {
+  isUserLoggedIn(): boolean {
+    const user = localStorage.getItem('email');
+
+    // return true if user not null
+    return !(user === null);
+  }
+
+  logOut(): void {
     localStorage.removeItem('email');
     localStorage.removeItem('token');
-    localStorage.removeItem('role');    
+    localStorage.removeItem('role');
     localStorage.removeItem('id');
     localStorage.removeItem('user');
 
--- src/app/Services/jwt-auth-http-interceptor.service.ts
+++ src/app/Services/jwt-auth-http-interceptor.service.ts
@@ -1,5 +1,6 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,15 @@ import { Injectable } from '@angular/core';
 export class JwtAuthHttpInterceptorService implements HttpInterceptor{
 
   constructor() { }
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     if (localStorage.getItem('email') && localStorage.getItem('token')) {
       req = req.clone({
         setHeaders: {
           Authorization: localStorage.getItem('token')
         }
-      })
+      });
     }
 
     return next.handle(req);
