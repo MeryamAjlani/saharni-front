12a9b894508f5fe6f3d119286fa71cf7d8292242
chaimakr <chaima050@gmail.com>
2022-02-15 21:39:00 +0100

    final touch

--- src/app/Layouts/client-layout/client-layout.component.ts
+++ src/app/Layouts/client-layout/client-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Models/user.model';
+import { AuthentificationService } from 'src/app/Services/authentification.service';
 
 @Component({
   selector: 'app-client-layout',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientLayoutComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private auth: AuthentificationService , private router : Router) { }
+  user : User
   ngOnInit(): void {
   }
-
+  logout(): void {
+    this.auth.logOut();
+    this.user = null;
+    this.router.navigate(['/']);
+  }
 }
